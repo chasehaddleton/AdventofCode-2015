fb975f7877f456c22dac955bc28f114cc6fa9e38
Chase Haddleton <chase@chasehaddleton.com>
2015-12-19 21:59:45 -0500

    Finished day three

--- src/dayThree/dayThree.java
+++ src/dayThree/dayThree.java
@@ -11,11 +11,14 @@ public class dayThree {
     public static void main(String args[]) {
         char[] input = null;
         boolean[][] house = new boolean[500][500];
-        int x = 250;
-        int y = 250;
+        int sx, sy;
+        int rx, ry;
         int sum = 0;
+        int i;
 
-        for (int i = 0; i < 500; i++) {
+        sx = sy = rx = ry = 250;
+
+        for (i = 0; i < 500; i++) {
             for (int k = 0; k < 500; k++) {
                 house[i][k] = false;
             }
@@ -23,38 +26,54 @@ public class dayThree {
 
         try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Chase\\Documents\\Programming\\Advent " +
                 "of Code\\src\\dayThree\\input.txt"))) {
-            String line = br.readLine();
-
-            input = line.toCharArray();
-
+            input = br.readLine().toCharArray();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
+
         for (char ch : input) {
-            switch (ch) {
-                case '<':
-                    y--;
-                    break;
-                case '>':
-                    y++;
-                    break;
-                case '^':
-                    x++;
-                    break;
-                case 'v':
-                    x--;
-                    break;
+            if (i++ % 2 == 0) {
+                switch (ch) {
+                    case '<':
+                        sy--;
+                        break;
+                    case '>':
+                        sy++;
+                        break;
+                    case '^':
+                        sx++;
+                        break;
+                    case 'v':
+                        sx--;
+                        break;
+                }
+                house[sx][sy] = true;
+            } else {
+                switch (ch) {
+                    case '<':
+                        ry--;
+                        break;
+                    case '>':
+                        ry++;
+                        break;
+                    case '^':
+                        rx++;
+                        break;
+                    case 'v':
+                        rx--;
+                        break;
+                }
+                house[rx][ry] = true;
             }
-            house[x][y] = true;
         }
 
-        for (int i = 0; i < 500; i++) {
+        for (i = 0; i < 500; i++) {
             for (int k = 0; k < 500; k++) {
                 if (house[i][k]) sum++;
             }
         }
 
-        System.out.println(sum);
+        System.out.println(sum); // Increment by 1 for just santa
     }
 }
